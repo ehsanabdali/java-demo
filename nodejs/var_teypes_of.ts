53873d5b0f2e708a66c7efde5883298192d56104
ehsanabdali <ehsan.abdali@gmail.com>
2023-03-03 20:01:24 +0500

    More variable initializing is added..

--- nodejs/var_teypes_of.ts
+++ nodejs/var_teypes_of.ts
@@ -1,5 +1,9 @@
-let testIntVar = 1;
-let testStringVar = "Test";
-console.log(typeof testIntVar);
-console.log(typeof testStringVar);
-console.log('Hi ,'+testStringVar+testIntVar);
+let testIntLet = 1;
+let testStringLet = "Test";
+var testIntVar = 10;
+var testStingVar = "String Stored variable initialised with var";
+const testIntConst = 100;
+const testStringConst = "String Initilised with Const"
+console.log(typeof testIntLet);
+console.log(typeof testStringLet);
+console.log('Variables initialized with let : Hi ,'+testStringLet+" ,"+testIntVar);

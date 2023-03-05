95fb072c11542299bd0bdee428254a222f4c952f
ehsanabdali <ehsan.abdali@gmail.com>
2023-03-05 14:32:16 +0500

    Conditional Loops

---
+++ nodejs/conditional_statements/if_prompt.js
@@ -0,0 +1,11 @@
+var askValue = prompt("Where does the Pope live?");
+var correctAnswer = "Pakistan";
+if (askValue == correctAnswer) {
+    alert("Correct!");
+}
+else if (askValue == "Pakistan") {
+    alert("Close!");
+}
+else {
+    alert("Wrong!!");
+}
---
+++ nodejs/conditional_statements/if_prompt.ts
@@ -0,0 +1,9 @@
+let askValue = prompt("Where does the Pope live?");
+let correctAnswer = "Pakistan";
+if (askValue == correctAnswer){
+    alert("Correct!");
+} else if(askValue=="Pakistan"){
+    alert("Close!");
+}else {
+    alert("Wrong!!");
+}

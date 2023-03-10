5d9a6a5e87578302be9d4dcfb41870f8886db177
ehsan-abdali <ehsan.abdali@gmail.com>
2023-03-10 10:32:40 +0500

    working on fuctions

---
+++ nodejs/functions/funk_brkt.js
@@ -0,0 +1,12 @@
+var rand1 = Math.floor(10 + Math.random() * 9);
+var rand2 = Math.floor(10 + Math.random() * 9);
+var rand3 = Math.floor(10 + Math.random() * 9);
+var rand4 = Math.floor(10 + Math.random() * 9);
+//
+console.log(rand1, rand2, rand3, rand4);
+if (rand1 > rand2) {
+    console.log(rand1, " > ", rand2);
+}
+else if (rand3 > rand4) {
+    console.log(rand3, ">", rand4);
+}
---
+++ nodejs/functions/funk_brkt.ts
@@ -0,0 +1,11 @@
+let rand1:number = Math.floor(10+Math.random()*9)
+let rand2:number = Math.floor(10+Math.random()*9)
+let rand3:number = Math.floor(10+Math.random()*9)
+let rand4:number = Math.floor(10+Math.random()*9)
+//
+console.log(rand1,rand2,rand3,rand4);
+if(rand1 > rand2){
+    console.log( rand1," > ",rand2);
+}else if(rand3 > rand4){
+    console.log(rand3,">",rand4)
+}

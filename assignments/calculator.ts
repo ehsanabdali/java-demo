d579f092d5905db263813d4f6f8eace3f5227276
ehsan-abdali <ehsan.abdali@gmail.com>
2023-04-01 02:00:13 +0500

    folder renamed

---
+++ assignments/calculator.ts
@@ -0,0 +1,50 @@
+/*------------------------------------------------------------------------------------
+Program: calculator.ts
+Function: Get three inputs 1. First Number 2. Second Number 3. Operator and 
+shows operation on the number na prompts for continue or not
+-------------------------------------------------------------------------------------*/
+import inquirer from "inquirer";    // inquirer module inlucded
+import chalk from "chalk";          // chalk module included
+
+console.log(chalk.bgBlue.red("C A L C U L A T O R"));
+
+let inputChoice={    // Choice Object initialized
+    control:'Y'
+};
+while(inputChoice.control=='y'|| inputChoice.control=='Y'){  // program will continue will input choice is other that y/Y
+
+    let FirstNum = await inquirer.prompt([{   // get input of first number
+        name: "Num1",
+        type: "number",
+        message: "Enter fisrt number: "}
+    ]);
+
+    let SecondNum = await inquirer.prompt([{   // get input of second number
+        name: "Num2",
+        type: "number",
+        message: "Enter second number: "}
+    ]);
+
+    let Operator = await inquirer.prompt([{  // get operator
+        name: "Option",
+        type: "string",
+        message: "Enter Opetrator (+,-,*,/): "}
+    ]);
+
+    if(Operator.Option=='+'){ // operation as per feeded operator
+        console.log("Sum of "+FirstNum.Num1+" and "+SecondNum.Num2+" is: "+(FirstNum.Num1+SecondNum.Num2));
+    }else if(Operator.Option=='-'){
+        console.log("Subtraction of "+FirstNum.Num1+" and "+SecondNum.Num2+" is: "+(FirstNum.Num1-SecondNum.Num2));
+    }else if(Operator.Option=='*'){
+        console.log("Product of "+FirstNum.Num1+" and "+SecondNum.Num2+" is: "+(FirstNum.Num1*SecondNum.Num2));
+    }else if(Operator.Option=='/'){
+        console.log(FirstNum.Num1+" Divided by "+SecondNum.Num2+" is: "+(FirstNum.Num1/SecondNum.Num2));
+    }else{
+        console.log(chalk.red.bold("Not valid operator..."));
+    }
+
+    inputChoice = await inquirer.prompt([{  // prompt for continue y/n
+        name: "control",
+        message: "More Calculations(y/n) : "}
+    ])
+}
---
+++ assignments/wovels_obj.ts
@@ -0,0 +1,24 @@
+// Program checks the input wheather it is Wovel or not
+// it prints that the input letter it Wovel otherwise it 
+// it is not Wovel.
+//-------------------------------------------------------
+import inquirer from "inquirer";
+import chalk from "chalk";
+//
+let inputLetter = {
+ letter: ''
+}
+while( inputLetter.letter !== '1'){
+
+    inputLetter = await inquirer.prompt([{
+        name: "letter",
+        message: "Type any letter (1 to quit): "}
+    ]);
+    let toLowerInput:string = inputLetter.letter.toLowerCase();
+if( toLowerInput == 'a' || toLowerInput == 'e'|| toLowerInput == 'i' || toLowerInput == 'o' ||  toLowerInput == 'u') {
+console.log(`\"${inputLetter.letter}\" is Wovel....`);
+}
+else{
+    console.log(`\"${inputLetter.letter}\" is not Wovel....`);
+}
+}

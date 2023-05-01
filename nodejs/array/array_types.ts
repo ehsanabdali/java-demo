4caa7c8393def08420568b8bbb4cdfec6745e018
ehsan-abdali <ehsan.abdali@gmail.com>
2023-05-01 12:06:57 +0500

    All editiging committed

---
+++ nodejs/array/array_types.ts
@@ -0,0 +1,10 @@
+let a:string[]=[];
+let b:number[]=[];
+a=["Pakistan"];
+ a.push("Ismamabad","Lahore");
+a.push("Karachi","Multan","Faisalabad");
+b.push(1,2,3,4,5);
+console.log(a);
+console.log(b);
+
+export{};
